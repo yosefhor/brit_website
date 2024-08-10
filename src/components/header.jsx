169d4a6ba452a10d9468e7b617ad4cb41111c165
yossef horvitz work pc <yossefhorvitz@gmail.com>
2024-08-10 23:55:02 +0300

    fix closing mobile version

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import logo from "../logo.jpg";
 import "bootstrap/dist/css/bootstrap.css";
 import { useTranslation } from 'react-i18next';
@@ -17,10 +18,20 @@ function Navbar() {
         localStorage.setItem('language', lng);
     };
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleLinkClick = () => {
+        setIsMenuOpen(false); // סוגר את התפריט אחרי לחיצה על קישור
+    };
+
     const options = [
         {
             value: 'de', label: (
-                <div className=" options">
+                <div className="options">
                     <img src={germanyFlag} alt="German flag" aria-label="German" />
                 </div>)
         },
@@ -82,15 +93,15 @@ function Navbar() {
     };
 
     return (
-        <header className=" sticky-top">
+        <header className="sticky-top">
             <div className="container-fluid bg-info-subtle justify-content-between align-items-center d-flex px-md-5" style={{ height: '100px' }}>
                 <div className="row align-items-center">
-                    <Link to="/" className=" col-auto">
+                    <Link to="/" className="col-auto" onClick={handleLinkClick}>
                         <img src={logo} alt="Logo" className="rounded-1 shadow" style={{ height: '6em' }} />
                     </Link>
                     <h1 className="text-danger fst-italic fs-2 col">{t('header.title')}</h1>
                 </div>
-                <div className="desktop-flags d-md-flex ">
+                <div className="desktop-flags d-md-flex">
                     <button style={{ boxShadow: 'none', border: 'none' }} className="btn d-flex flex-column align-items-center" onClick={() => changeLanguage('de')}><img alt="German flag" src={germanyFlag} />Deutsch</button>
                     <button style={{ boxShadow: 'none', border: 'none' }} className="btn d-flex flex-column align-items-center" onClick={() => changeLanguage('en')}><img alt="USA flag" src={usaFlag} />English</button>
                     <button style={{ boxShadow: 'none', border: 'none' }} className="btn d-flex flex-column align-items-center" onClick={() => changeLanguage('he')}><img alt="Israel flag" src={israelFlag} />עברית</button>
@@ -98,17 +109,17 @@ function Navbar() {
                 <Select className="mobile-flags" options={filteredOptions} defaultValue={options.find(option => option.value === savedLanguage)} styles={customStyles} isSearchable={false} components={{ IndicatorSeparator: () => null }} onChange={(e) => changeLanguage(e.value)} />
             </div>
             <div className="bg-light">
-                <nav className=" ms-sm-5 navbar navbar-expand-sm px-2">
-                    <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+                <nav className="ms-sm-5 navbar navbar-expand-sm px-2">
+                    <button className="navbar-toggler" type="button" onClick={handleToggleMenu} data-bs-toggle="collapse" data-bs-target="#navbarContent">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarContent">
-                        <div className=" navbar-nav ">
-                            <Link className="nav-link active mx-md-2" aria-current="page" to="/">{t('header.home')}</Link>
-                            <Link className="nav-link mx-md-2" to="aboutMe">{t('header.aboutMe')}</Link>
-                            <Link className="nav-link mx-md-2" to="aboutBritMilah">{t('header.aboutBritMilah')}</Link>
-                            <Link className="nav-link mx-md-2" to="costs">{t('header.costs')}</Link>
-                            <Link className="nav-link mx-md-2" to="contact">{t('header.contact')}</Link>
+                    <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarContent">
+                        <div className="navbar-nav">
+                            <Link className="nav-link active mx-md-2" aria-current="page" to="/" onClick={handleLinkClick}>{t('header.home')}</Link>
+                            <Link className="nav-link mx-md-2" to="aboutMe" onClick={handleLinkClick}>{t('header.aboutMe')}</Link>
+                            <Link className="nav-link mx-md-2" to="aboutBritMilah" onClick={handleLinkClick}>{t('header.aboutBritMilah')}</Link>
+                            <Link className="nav-link mx-md-2" to="costs" onClick={handleLinkClick}>{t('header.costs')}</Link>
+                            <Link className="nav-link mx-md-2" to="contact" onClick={handleLinkClick}>{t('header.contact')}</Link>
                         </div>
                     </div>
                 </nav>
@@ -116,4 +127,5 @@ function Navbar() {
         </header>
     );
 }
-export default Navbar
+
+export default Navbar;
